648d27b4f8f478fe177b3b41a41cd9dc6ccbe4d6
allen kao <kaoluiben@gmail.com>
2022-05-09 17:18:34 +0800

    create

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-function helloWorld () { // 創建函式
-    const content = 'helloWorld' // 給變數內容
-    console.log(content) // 用日誌將內容印出來
+require('dotenv').config(); //載入.env環境檔
+function getEnvVariable  () { // 創建函式
+    const env_variable= process.env.time // 取出環境變數
+    console.log(env_variable) // 用日誌將內容印出來
 }
-helloWorld() // 觸發
+getEnvVariable () // 觸發
